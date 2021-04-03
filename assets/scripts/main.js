4e488d0cf0267d91cd0af3aee686da5554748b96
jakelashbrook <jakeashleylashbrook@gmail.com>
2021-04-03 11:56:19 +0000

    Back to the top btn JS functions Added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -118,3 +118,25 @@ $(document).ready(function() {
 
 });
 
+
+// Start of code based on W3schools Back to the top button page
+//Get the back to top button
+let mybutton = document.getElementById("back-to-top-btn");
+
+// When the user scrolls down 60px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+// button appears after page has been scrolled
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// End of code based on w3 Schools
